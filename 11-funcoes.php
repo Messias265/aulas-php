dd26eb37e753138b8246e119e01be1e25e8ce210
Messias265 <messiaskassiano92@gmail.com>
2026-08-12 11:33:27 -0300

    funcoes multiplicar

--- 11-funcoes.php
+++ 11-funcoes.php
@@ -87,13 +87,33 @@
         } ?>
         <p>Numero 10: <?= verificarNegativo(10) ?></p>
         <p>Numero -10: <?=  verificarNegativo(-10) ?></p>
-        <p>Teste para erro: <?= verificarNegativo("") ?></p>
+     
 
     </div>
             <!-- exclua APÓs o teste -->
              
-<!-- <p>Teste para erro: <?= verificarNegativo("teste") ?></p> -->
- 
+<!-- <p>Teste para erro: < verificarNegativo("teste") ?></p> -->
+
+<hr>
+<h2>Função anônima (ou lambda)</h2>
+<?php 
+$multiplicar = function( float $valor1, float $valor2):float {
+    return $valor1 * $valor2;
+};
+
+?>
+<!-- Repare no uso do $ junto com o nome da função: -->
+<p>Exemplo: <?= $multiplicar(10, 2) ?></p>
+
+<hr>
+
+<h2>Arrow Function</h2>
+<?php // Repare que omitimos as chaves e o return
+$subtrair = fn (float $valor1, float $valor2):float => $valor1 - $valor2; 
+?>
+<p>Exemplo: <?= $subtrair (10, 1) ?></p>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
