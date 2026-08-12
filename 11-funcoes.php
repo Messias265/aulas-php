2114a7f18f9cedff83a4c1850046188fa527a1e5
Messias265 <messiaskassiano92@gmail.com>
2026-08-12 11:23:11 -0300

    funcoes

--- 11-funcoes.php
+++ 11-funcoes.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,71 +8,93 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+
 <body>
-    <div class="container"> 
+    <div class="container">
         <h1>Trabalhando com funções</h1>
         <hr>
     </div>
-    
+
     <h2>Função como procedimento (ou sub-rotina)</h2>
     <p>Procedimentos não retornam nada.</p>
 
-    <?php 
-    function exibirDadosDoAutor(){
+    <?php
+    function exibirDadosDoAutor()
+    {
         echo "<h4>Fulano de Tal <h4>";
         echo "<p> Aplicação <b>Back-End</b> como exemplo</p>";
     }
     ?>
-        <h3>Chamar/Invocar a função/procedimento</h3>
+    <h3>Chamar/Invocar a função/procedimento</h3>
+    <?php exibirDadosDoAutor() ?>
+
+    <div>
         <?php exibirDadosDoAutor() ?>
 
-        <div>
-            <?php exibirDadosDoAutor() ?>
-       
 
         <hr>
 
         <h2>Função com parâmetros (ou argumentos)</h2>
-        <?php 
-        function somar( $valor1,  $valor2){
+        <?php
+        function somar($valor1,  $valor2)
+        {
             $total = $valor1 + $valor2;
             return $total;
         } ?>
         <h3>Chamada/retorno da função somar</h3>
-        <p>Resultado 1: <?= somar(10, 20)?></p>
+        <p>Resultado 1: <?= somar(10, 20) ?></p>
         <p>Resultado 2: <?= somar(1234, 250) ?></p>
         <p>Resultado 3: <?= somar(2, 10.5) ?></p>
 
-        <?php 
+        <?php
         // Variável de escopo GLOBAL
         $precoProdutoA = 250;
         $precoProdutoB = 300;
 
         /* Podemos passar valores de outras variáveis para os parâmetros de uma função*/
-        $resultadoProdutos = somar ($precoProdutoA, $precoProdutoB);
+        $resultadoProdutos = somar($precoProdutoA, $precoProdutoB);
         ?>
         <p>Resultado 4: <?= $resultadoProdutos ?></p>
 
         <!-- Utilizando função como parte de condição de um if -->
-         <?php if(somar(100, 500) > 1200): ?>
+        <?php if (somar(100, 500) > 1200): ?>
             <p class="text-success">Meta atingida!</p>
-            <?php else: ?>
-                <p class="text-danger">Não foi desta vez!</p>
-                <?php endif; ?>
+        <?php else: ?>
+            <p class="text-danger">Não foi desta vez!</p>
+        <?php endif; ?>
 
-                <hr>
-                <h2>Função com parâmetros opcionais</h2>
-                <?php 
-                /* Neste caso, deixamos o parâmetro pessoa com um valor padrão (no exemplo, uma string vazia) */
-                function exibirMensagem($mensagem, $pessoa = "visitante"){
-                    return "Olá, $mensagem $pessoa";
-                }
-                ?>
-                <p>Saudação 1: <?= exibirMensagem("Bom tarde", "Samuel") ?></p>
-                <p>Saudação 2: <?= exibirMensagem("bom dia") ?></p>
- </div>
+        <hr>
+        <h2>Função com parâmetros opcionais</h2>
+        <?php
+        /* Neste caso, deixamos o parâmetro pessoa com um valor padrão (no exemplo, uma string vazia) */
+        function exibirMensagem($mensagem, $pessoa = "visitante")
+        {
+            return "Olá, $mensagem $pessoa";
+        }
+        ?>
+        <p>Saudação 1: <?= exibirMensagem("Bom tarde", "Samuel") ?></p>
+        <p>Saudação 2: <?= exibirMensagem("bom dia") ?></p>
+        <hr>
 
+        <h2> Função com indução de tipos de dados</h2>
+        <p>Nesta abordagem, definimos tipos dfe dados para os parâmetros e para o retorno da função.</p>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <?php
+        function verificarNegativo(int $valor): string
+        {
+            if ($valor < 0) return "é negativo";
+            return "não é negativo";
+        } ?>
+        <p>Numero 10: <?= verificarNegativo(10) ?></p>
+        <p>Numero -10: <?=  verificarNegativo(-10) ?></p>
+        <p>Teste para erro: <?= verificarNegativo("") ?></p>
+
+    </div>
+            <!-- exclua APÓs o teste -->
+             
+<!-- <p>Teste para erro: <?= verificarNegativo("teste") ?></p> -->
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>
