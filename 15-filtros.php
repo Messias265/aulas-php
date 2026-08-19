1bcc2b2ef5082b61f67a0e623b20677783173cae
Messias265 <messiaskassiano92@gmail.com>
2026-08-19 10:21:48 -0300

    float e int

--- 15-filtros.php
+++ 15-filtros.php
@@ -37,6 +37,70 @@ $redeSocialValida = filter_var($redeSocial, FILTER_VALIDATE_URL);
 if($redeSocialValida): ?>
     <a href="<?php $redeSocial ?>" class="btn btn-info">Me siga no LinkedIn</a>
 <?php endif; ?>
+
+    <hr>
+
+    <h3>FILTER_SANITIZE_EMAIL</h3>
+<?php 
+$contato = "tiago.bsantos @sp.;senac.br ( >";
+$contatoSanitizado = filter_var($contato, FILTER_SANITIZE_EMAIL);
+?>
+    <p>Contato <b>sem</b> sanitização: <?= $contato ?></p>
+    <p>Contato <b>com</b> sanitização: <?= $contatoSanitizado ?></p>
+
+    <h3>FILTER_SANITIZE_FULL_SPECIAL_CHARS</h3>
+<?php 
+// Simulando uma entrada de dados de código HTML
+$nomeCompleto = "<img src='https://ogimg.infoglobo.com.br/rioshow/25088054-e75-c90/FT1086A/pacoca-caseira.jpg'>";
+
+$nomeCompletoSanitizado = filter_var(
+    $nomeCompleto, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+?>
+    
+    <p>Nome informado: <?= $nomeCompletoSanitizado ?></p>
+
+<?php 
+// Simulando de um ataque de injeção de código JS (XSS - Cross Site Scripting) 
+$ataqueXSS = "<script>location = 'https://sp.senac.br'</script>";
+?>
+    <p>Teste: <?= filter_var($ataqueXSS, FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?></p>
+
+    <h3>htmlspecialchars()</h3>
+    <p>Pode ser usado como alternativa ao filtro.</p>
+<?php 
+$nomeCompletoCorrigido = htmlspecialchars($nomeCompleto);
+$ataqueEvitado = htmlspecialchars($ataqueXSS);
+?>
+    <p>Nome completo corrigido: <?= $nomeCompletoCorrigido ?></p>
+    <p>Ataque evitado: <?= $ataqueEvitado ?></p>
+
+    <h3>FILTER_SANITIZE_NUMBER_INT</h3>
+<?php 
+$idade = "Tenho 15 anos";
+$idade = filter_var($idade, FILTER_SANITIZE_NUMBER_INT);
+?>
+
+    <p>Idade: <?= $idade ?></p>
+
+    <h3>FILTER_SANITIZE_NUMBER_FLOAT</h3>
+
+<?php 
+$precoInicial = "R$ 1000.78";
+$desconto = "R$ 500.30";
+
+$precoInicial = filter_var(
+   $precoInicial, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION
+);
+
+$desconto = filter_var(
+   $desconto, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION
+);
+
+$precoFinal = $precoInicial - $desconto;
+?>
+    <p>Preço inicial: <?= $precoInicial ?></p>
+    <p>Desconto de: <?= $desconto ?></p>
+    <p>Preço final: <?= $precoFinal ?></p>
 </div>
     
 
